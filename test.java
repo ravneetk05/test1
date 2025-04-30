bd2bef0ec9df7a56432ab7c5ea5cf72a0275a8a2
ravneetk05 <157576316+ravneetk05@users.noreply.github.com>
2025-04-30 17:38:36 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class test {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right = null;
+        }
+    }
+    public static void inorder(Node root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        
+        Node root = new Node(1);
+        root.right = new Node(2);
+        root.right.left = new Node(3);
+        inorder(root); 
+    }
+}
